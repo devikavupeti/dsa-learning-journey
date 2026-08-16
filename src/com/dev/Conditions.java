729548a24f60ead23b927dea6d8b3df83be7e257
Devika <vupetidevika@gmail.com>
2026-08-16 15:58:10 +0530

    Conditional logic

---
+++ src/com/dev/Conditions.java
@@ -0,0 +1,12 @@
+package com.dev;
+
+public class Conditions {
+    public static void main(String[] args) {
+        int age = 20;
+        if (age < 18) {
+            System.out.println("You are not eligible to vote");
+        } else {
+            System.out.println("you are eligible to vote");
+        }
+    }
+}
