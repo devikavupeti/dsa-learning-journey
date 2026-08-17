0ba4d55375dcb03f286cd99f6db0601159def7ca
Devika <vupetidevika@gmail.com>
2026-08-17 14:47:25 +0530

    Add files via upload

---
+++ src/com/dev/Fibonacci.java
@@ -0,0 +1,23 @@
+package com.dev;
+
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    static void main() {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+
+        while (count <= n) {
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count++;
+        }
+
+        System.out.println(b);
+    }
+}
